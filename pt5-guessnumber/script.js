f9476fc20840c547f1b77fa8f8f4842e013a5886
Epulon <nvicic@protonmail.com>
2021-06-03 00:21:45 +0100

    Reinit

--- pt5-guessnumber/script.js
+++ pt5-guessnumber/script.js
@@ -2,8 +2,3 @@
 
 //Selecting DOM element
 console.log(document.querySelector(".message").textContent);
-//git commit
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
