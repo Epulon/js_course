e31ac1a8c53f5002cf05522d141603dd805f8587
Epulon <nvicic@protonmail.com>
2021-12-26 17:23:16 +0100

    L72

--- pt5-guessnumber/script.js
+++ pt5-guessnumber/script.js
@@ -1,4 +1,12 @@
 "use strict";
 
 //Selecting DOM element
-console.log(document.querySelector(".message").textContent);
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 19;
+
+document.querySelector('.guess').value = 47;
+console.log(document.querySelector('.guess').value);
