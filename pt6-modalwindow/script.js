f0ab5be8c70b648e2764458a1fe741d8951157d4
Epulon <nvicic@protonmail.com>
2022-01-27 08:43:49 +0100

    L80

--- pt6-modalwindow/script.js
+++ pt6-modalwindow/script.js
@@ -5,6 +5,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+/*_______________Open Modal Window_______________*/
 for (let i =0; i < btnsOpenModal.length; i++) 
-    console.log(btnsOpenModal[i].textcontent);
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+/*_______________Close Modal Window_______________*/
+btnCloseModal.addEventListener('click', closeModal);    
+overlay.addEventListener('click', closeModal)
